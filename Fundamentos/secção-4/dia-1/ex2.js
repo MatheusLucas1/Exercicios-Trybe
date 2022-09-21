74d0d157ab7f1fead6198b3f9cf7a777383198ff
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-09-21 16:42:58 -0300

    ex3

---
+++ Fundamentos/secção-4/dia-1/ex2.js
@@ -0,0 +1,16 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimeter = (2*base) + (2*heigth)
+
+console.log("o área do retângulo é :", area);
+console.log("o perímetro do retângulo é :", perimeter)
---
+++ Fundamentos/secção-4/dia-1/ex3.js
@@ -0,0 +1,11 @@
+const nota = 50;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado(a)")
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nodda lista de espera")
+}else{
+    console.log("Você foi reprovado")
+}
+
