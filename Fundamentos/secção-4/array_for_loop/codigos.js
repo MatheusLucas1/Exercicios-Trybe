e8c9ecfc3aafe7a01ca37d21e52f7adec1a716d7
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-09-23 08:55:03 -0300

    ex 5

--- Fundamentos/secção-4/array_for_loop/codigos.js
+++ Fundamentos/secção-4/array_for_loop/codigos.js
@@ -1,10 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maior = numbers[0] 
 
-for (let i = 0 ; i < numbers.length; i++){
-    soma += numbers[i];
+for(let i = 0; i<numbers.length; i++){
+    if (numbers[i]> maior){
+        maior = numbers[i]
+    }
 }
-
-media = soma/numbers.length;
-    
-console.log(media)
+console.log(maior)
