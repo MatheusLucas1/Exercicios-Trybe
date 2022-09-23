2b8b4d00ea95253491b9d70d6ed9a92f7d1d81e7
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-09-23 10:53:27 -0300

    ex 8

--- Fundamentos/secção-4/array_for_loop/codigos.js
+++ Fundamentos/secção-4/array_for_loop/codigos.js
@@ -1,9 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = numbers[0];
+let array = [];
 
-for(let i = 0; i<numbers.length; i++){
-    if (numbers[i]< menor){
-        menor = numbers[i];
-    }
+for (let i = 0; i<26; i++){
+    array.push(i);
 }
-console.log(menor)
+
+console.log(array)
