491d6558fa3efbe1eec5889df7766425f4e86235
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-09-23 10:50:09 -0300

    ex 7

--- Fundamentos/secção-4/array_for_loop/codigos.js
+++ Fundamentos/secção-4/array_for_loop/codigos.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-impares = [];
+let menor = numbers[0];
 
 for(let i = 0; i<numbers.length; i++){
-    if ((numbers[i]%2) != 0){
-        impares.push(numbers[i]);
+    if (numbers[i]< menor){
+        menor = numbers[i];
     }
 }
-console.log(impares.length)
+console.log(menor)
