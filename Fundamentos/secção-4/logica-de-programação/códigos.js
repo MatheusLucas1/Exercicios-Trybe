abb76287198cca157e1bfa32c6d762320585fa1e
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-09-26 09:11:55 -0300

    menorMaior palavra

--- Fundamentos/secção-4/logica-de-programação/códigos.js
+++ Fundamentos/secção-4/logica-de-programação/códigos.js
@@ -1,6 +1,19 @@
-let word = 'tryber';
-let inversa = '';
-for (let i = 0; i <= word.length ; i+=1 ) {
-    inversa += word[word.length - 1  -  i]
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+maiorPalavra = array[0];
+menorPalavra = array[0];
+
+for(let i = 0; i<array.length; i+=1) {
+    if(array[i].length > maiorPalavra.length) {
+        maiorPalavra = array[i]
+    }
 }
-console.log(inversa);
+
+for(let i = 0 ; i < array.length ; i+=1) {
+    if (array[i].length< menorPalavra.length) {
+        menorPalavra = array[i]
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra)
