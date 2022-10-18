0ba958e8724fc9da70ee772726963e6f96ca1057
Matheus Lucas <matheus.lucas1105@gmail.com>
2022-10-18 10:55:41 -0300

    ex1

--- Fundamentos/secção-7/7.1/script.js
+++ Fundamentos/secção-7/7.1/script.js
@@ -1,13 +1,7 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-testingScope(true);-
+const sortOddsAndEvens = () => {
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
